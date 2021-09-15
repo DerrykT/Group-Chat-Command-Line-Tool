b8e84a4dd34972da5843a87c9a3b61b49475dc8d
Derryk <taylordm@clarkson.edu>
2021-09-14 21:37:06 -0400

    Set up class hierarchy and created variables for classes in data package.

---
+++ src/data/ClackData.java
@@ -0,0 +1,9 @@
+package data;
+
+import java.util.Date;
+
+public class ClackData {
+    private String userName;
+    private int type;
+    private Date date;
+}
---
+++ src/data/FileClackData.java
@@ -0,0 +1,6 @@
+package data;
+
+public class FileClackData extends ClackData {
+    private String fileName;
+    private String fileContents;
+}
